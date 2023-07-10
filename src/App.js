56405924c13ee6f088ba859977dbc0e931b41eab
Kun <kunmaxstudio@gmail.com>
2023-07-11 02:08:37 +0700

    add new project jobboard

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Loading Project...
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/kunmaxstudio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Copyright Kun 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
